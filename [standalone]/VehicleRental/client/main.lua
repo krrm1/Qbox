4c83ed4d0c3d7be84abaa1daed49acfd2322f125
kareem_105 <89742984+krrm1@users.noreply.github.com>
2025-11-29 22:52:18 +0200

    update

--- [standalone]/VehicleRental/client/main.lua
+++ [standalone]/VehicleRental/client/main.lua
@@ -1,7 +1,30 @@
 local pedsSpawned = false
 local inRangeRental = false
 
-local function SpawnVehicleRented(rentalname, VehicleModel, VehicleSpawn)
+local function createBlip(data)
+    local blip = AddBlipForCoord(data.coords.x, data.coords.y, data.coords.z)
+    SetBlipSprite(blip, data.blip.sprite or 1)
+    SetBlipDisplay(blip, data.blip.display or 4)
+    SetBlipScale(blip, data.blip.scale or 1.0)
+    SetBlipColour(blip, data.blip.colour or 1)
+    SetBlipAsShortRange(blip, config.bilpsSettings.shortRange or false)
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentString(data.blip.label)
+    EndTextCommandSetBlipName(blip)
+    return blip
+end
+
+local function deleteBlips()
+    if not blips then return end
+    for i = 1, #blips do
+        local blip = blips[i]
+        if DoesBlipExist(blip) then
+            RemoveBlip(blip)
+        end
+    end
+    blips = {}
+end
+local function SpawnVehicleRented(VehicleModel, VehicleSpawn)
     local veh = VehicleModel
     local spawn = VehicleSpawn
         local vehNetId = lib.callback.await('VehicleRental:rent', false, veh, spawn)
@@ -24,17 +47,6 @@ local function SpawnVehicleRented(rentalname, VehicleModel, VehicleSpawn)
         SetVehRadioStation(veh, 'OFF')
 end
 
-local function deletePeds()
-    if not config.peds or not next(config.peds) or not pedsSpawned then return end
-    for i = 1, #config.peds do
-        local current = config.peds[i]
-        if current.pedHandle then
-            DeletePed(current.pedHandle)
-        end
-    end
-end
-
-
 local function spawnPeds()
     if not config.peds or not next(config.peds) or pedsSpawned then return end
     for i = 1, #config.peds do
@@ -49,6 +61,7 @@ local function spawnPeds()
         SetBlockingOfNonTemporaryEvents(ped, true)
         TaskStartScenarioInPlace(ped, current.scenario, 0, true)
         pedsSpawned = true
+        
         if config.useTarget  then
             if pedsSpawned == true then
                 exports.ox_target:addLocalEntity(ped, {{ 
@@ -63,10 +76,10 @@ local function spawnPeds()
 
                         for veh, desc in pairs(current.VehiclesList) do
                             options[#options+1] = {
-                                title = veh,
-                                description = desc,
+                                title = desc.label,
+                                description = desc.price .. ' $',
                                 onSelect = function()
-                                    SpawnVehicleRented(current.rentalname, veh, current.VehicleSpawn)
+                                    SpawnVehicleRented(veh, current.VehicleSpawn)
                                 end
                             }
                         end
@@ -128,9 +141,20 @@ local function spawnPeds()
                 })
             end
         end
+        if not config.bilpsSettings.showBlip or not current.blip then return end
+        createBlip({blip = current.blip, coords = current.coords})
     end
 end
 
+local function deletePeds()
+    if not config.peds or not next(config.peds) or not pedsSpawned then return end
+    for i = 1, #config.peds do
+        local current = config.peds[i]
+        if current.pedHandle then
+            DeletePed(current.pedHandle)
+        end
+    end
+end
 
 RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
     spawnPeds()
@@ -143,10 +167,12 @@ end)
 
 RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
     deletePeds()
+    deleteBlips()
 end)
 
 AddEventHandler('onResourceStop', function(resource)
     if resource ~= cache.resource then return end
-    deletePeds()
-    inRangeRental = false 
+        deletePeds()
+        deleteBlips()
+    inRangeRental = false
 end)
--- [standalone]/VehicleRental/config.lua
+++ [standalone]/VehicleRental/config.lua
@@ -2,52 +2,118 @@ config = {}
 
 config.useTarget = true
 config.debugPoly = true
+config.bilpsSettings = {
+    showBlip = true,
+    shortRange = true,
+}
 
 config.peds = {{
-        VehicleSpawn = vec4(300.28, -330.15, 44.92, 67.71),
-        VehiclesList = {
-            t20 = 't20 | Super Car Super fast',
-            casco = 'casco | Calssic Car Sport',
-            aleutian = 'aleutian | New Body Style SUV'
-        },
         model = 's_m_o_busker_01',
+        scenario = 'WORLD_HUMAN_CLIPBOARD_FACILITY',
         coords = vec4(296.58, -327.2, 43.92, 152.66),
-        scenario = 'WORLD_HUMAN_CLIPBOARD_FACILITY',
+        VehicleSpawn = vec4(300.28, -330.15, 44.92, 67.71),
         rentalname = 'rental 1',
+        VehiclesList = {
+            ["cypher"] = {
+                label = "cypher | sport",
+                price = 500
+            },
+            ["elegy"] = {
+                label = "elegy | sport",
+                price = 500
+            },
+            ["paragon"] = {
+                label = "paragon | sport",
+                price = 500
+            },
+            ['paragon3'] = {
+                label = "paragon3 | sport",
+                price = 500
+            },
+        },
+        blip = {
+        label = 'Vehicle Rental 1',
+            sprite = 810,
+            display = 4,
+            scale = 0.65,
+            colour = 2,
+        },
         zoneOptions = { -- Used for when UseTarget is false
             length = 3.0,
             width = 3.0,
-            debugPoly = true,
-        }
+            debugPoly = config.debugPoly,
+        },
     },{
-        VehicleSpawn = vec4(295.87, -343.17, 44.92, 69.83),
-        VehiclesList = {
-            t20 = 't20',
-            brigham = 'brigham',
-            blade = 'blade'
-        },        model = 's_m_o_busker_01',
+        model = 's_m_o_busker_01',
+        scenario = 'WORLD_HUMAN_CLIPBOARD_FACILITY',
         coords = vec4(292.16, -339.43, 43.92, 156.66),
-        scenario = 'WORLD_HUMAN_CLIPBOARD_FACILITY',
+        VehicleSpawn = vec4(295.87, -343.17, 44.92, 69.83),
         rentalname = 'rental 2',
+        VehiclesList = {
+            ["sentinel4"] = {
+                label = "sentinel4 | sport",
+                price = 500
+            },
+            ["stingertt"] = {
+                label = "stingertt | sport",
+                price = 500
+            },
+            ["flashgt"] = {
+                label = "flashgt | sport",
+                price = 500
+            },
+            ['banshee3'] = {
+                label = "banshee3 | sport",
+                price = 500
+            },
+        },
+        blip = {
+        label = 'Vehicle Rental 2',
+            sprite = 810,
+            display = 4,
+            scale = 0.65,
+            colour = 2,
+        },
         zoneOptions = { -- Used for when UseTarget is false
             length = 3.0,
             width = 3.0,
-            debugPoly = true,
+            debugPoly = config.debugPoly ,
         }
     },{
-        VehicleSpawn = vec4(288.91, -325.88, 44.92, 249.85),
-        VehiclesList = {
-            t20 = 't20',
-            vader = 'vader',
-            apc = 'apc'
-        },        model = 's_m_o_busker_01',
-        coords = vec4(293.29, -325.28, 43.92, 155.41),
+        model = 's_m_o_busker_01',
         scenario = 'WORLD_HUMAN_CLIPBOARD_FACILITY',
+        coords = vec4(293.29, -325.28, 43.92, 155.41),
+        VehicleSpawn = vec4(288.91, -325.88, 44.92, 249.85),
         rentalname = 'rental 3',
+        VehiclesList = {
+            ["rt3000"] = {
+                label = "rt3000 | sport",
+                price = 500
+            },
+            ["envisage"] = {
+                label = "envisage | sport",
+                price = 500
+            },
+            ["omnisegt"] = {
+                label = "omnisegt | sport",
+                price = 500
+            },
+            ['r300'] = {
+                label = "r300 | sport",
+                price = 500
+            },
+        },
+        blip = {
+        label = 'Vehicle Rental 3',
+            sprite = 810,
+            display = 4,
+            scale = 0.65,
+            colour = 2,
+        },
         zoneOptions = { -- Used for when UseTarget is false
             length = 3.0,
             width = 3.0,
-            debugPoly = true,
+            debugPoly = config.debugPoly,
         }
     }
 }
